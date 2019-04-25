1f06f1529e8394d66a17be6471d44f7256a32ef1
Devon Smith <dsmith@Devons-MacBook-Pro.local>
2019-04-25 10:47:47 -0600

    Add DB table creation and data input methods

--- dataGenerator.py
+++ dataGenerator.py
@@ -2,6 +2,7 @@
 import random
 import json
 from datetime import timedelta, datetime, date
+import pymysql
 
 
 def dataGen(rData):
@@ -22,22 +23,60 @@ def dataGen(rData):
 
 
 def dataToJson(rData):
-    "Sends the data to a JSON file"
+    """Sends the data to a JSON file"""
     # Outputs objects to dataPoints.json file
     with open("dataPoints.json", "w") as outfile:
         json.dump(rData, outfile, indent=4)
 
 
-def dataToDb():
-    "Sends the data to a DB"
-    print("Not Implemented yet")
+def createDbTable():
+    """Sends the data to a DB
+
+    To start mariaDB on Mac, use 'mysql.server start'
+    To log in, type 'mysql -uroot'
+    """
+
+    testDataTable = """CREATE TABLE testdata(
+        id INT NOT NULL AUTO_INCREMENT,
+        result INT NOT NULL,
+        build_date VARCHAR(10),
+        percent_deviation FLOAT NOT NULL,
+        PRIMARY KEY ( id ) )"""
+
+    db = pymysql.connect("localhost", "dsmith", "QWer!@34", "dataplot")
+    cursor = db.cursor()
+    cursor.execute("DROP TABLE IF EXISTS testdata")
+    cursor.execute(testDataTable)
+
+    db.close()
+
+
+def insertTableData(rData):
+    """Inserts data into the testData table"""
+
+    db = pymysql.connect("localhost", "dsmith", "QWer!@34", "dataplot")
+    cursor = db.cursor()
+
+    for x in range(len(rData)):
+        cursor.execute("""INSERT INTO testdata (result,
+                        build_date,
+                        percent_deviation) VALUES
+                        ({result},
+                        {build_date},
+                        {percent_deviation})""".format(result=rData["Build" +
+                                                                    str(x)]["Result"],
+                                                                build_date=rData["Build"+str(x)]["Date"],
+                                                                percent_deviation=rData["Build" + str(x)]["PercentDeviation"]))
+    db.close()
 
 
 def main():
-    dataDict = {}
+    dataDict={}
 
     dataGen(dataDict)
-    dataToJson(dataDict)
+    # dataToJson(dataDict)
+    createDbTable()
+    insertTableData(dataDict)
 
 
 if __name__ == '__main__':
