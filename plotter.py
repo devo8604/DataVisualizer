015b867d610e391b47b881502420fea6e0d392fe
Devon Smith <dsmith@Devons-MacBook-Pro.local>
2019-04-24 19:38:27 -0600

    Cleaned up code to PEP8 and added error handling

--- plotter.py
+++ plotter.py
@@ -9,18 +9,22 @@ resultArr = []
 deviationArr = []
 dateArr = []
 
-#Read the JSON data
-with open('dataPoints.json','r') as json_file:
-    dataDict = json.load(json_file) 
+# Read the JSON data
+try:
+    with open('dataPoints.json', 'r') as json_file:
+        dataDict = json.load(json_file)
+except IOError as e:
+    print(e)
+    exit(1)
 
-#Iterates through dict and assigns the elements to the proper array for plotting
+# Iterates through dict and assigns the elements to the proper array for plot
 for x in range(len(dataDict)):
     resultArr.append(dataDict["Build"+str(x)]["Result"])
-    #Must convert the date from string back to date
+    # Must convert the date from string back to date
     dateArr.append(datetime.strptime(dataDict["Build"+str(x)]["Date"], '%Y-%m-%d'))
     deviationArr.append(dataDict["Build"+str(x)]["PercentDeviation"])
 
-#Dont need this anymore, set the mem free...
+# Dont need this anymore, set the mem free...
 dataDict.clear()
 
 convertedDates = mdates.datestr2num(dateArr)
