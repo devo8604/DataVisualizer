fb4aa05f8bce10df7e2d4ab65abebb9adc0ac6dc
Devon Smith <dsmith@Devons-MacBook-Pro.local>
2019-04-25 08:11:02 -0600

    Refactoring into function

--- dataGenerator.py
+++ dataGenerator.py
@@ -3,23 +3,41 @@ import random
 import json
 from datetime import timedelta, datetime, date
 
-"""Control number that the deviation percentage is
-calculated from and empty dict Var"""
-CONTROL = 10
-dataDict = {}
-randDate = (date.today() - (timedelta(days=100)))
 
-for x in range(100):
-    randResult = random.randint(5, 15)
-    randDate += (timedelta(days=1))
-    deviationCalc = 100 * (abs((randResult - CONTROL)) / CONTROL)
+def dataGen(rData):
+    """Control number that the deviation percentage is
+    calculated from and empty dict Var"""
+    CONTROL = 10
+    randDate = (date.today() - (timedelta(days=100)))
 
-    # Create the Datapoint "object" and appends it to the dict
-    dataDict.update({"Build" + str(x): {"Result": randResult,
-                                        "Date": datetime.strftime(randDate, '%Y-%m-%d'),
-                                        "PercentDeviation": deviationCalc}})
+    for x in range(100):
+        randResult = random.randint(5, 15)
+        randDate += (timedelta(days=1))
+        deviationCalc = 100 * (abs((randResult - CONTROL)) / CONTROL)
+
+        # Create the Datapoint "object" and appends it to the dict
+        rData.update({"Build" + str(x): {"Result": randResult,
+                                         "Date": datetime.strftime(randDate, '%Y-%m-%d'),
+                                         "PercentDeviation": deviationCalc}})
 
 
-# Outputs objects to dataPoints.json file
-with open("dataPoints.json", "w") as outfile:
-        json.dump(dataDict, outfile, indent=4)
+def dataToJson(rData):
+    "Sends the data to a JSON file"
+    # Outputs objects to dataPoints.json file
+    with open("dataPoints.json", "w") as outfile:
+        json.dump(rData, outfile, indent=4)
+
+
+def dataToDb():
+    "Sends the data to a DB"
+    print("Not Implemented yet")
+
+
+def main():
+    dataDict = {}
+
+    dataGen(dataDict)
+    dataToJson(dataDict)
+
+
+main()
