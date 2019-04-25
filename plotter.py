98069879d0abce7401f4226506b88c1db34f6edd
Devon Smith <dsmith@Devons-MacBook-Pro.local>
2019-04-25 13:45:26 -0600

    Refactor into functions, added DB stuff

--- plotter.py
+++ plotter.py
@@ -3,38 +3,67 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import json
 
+# Custom Modules
+import dataGenerator
+
 # Variable List
 dataDict = {}
 resultArr = []
 deviationArr = []
 dateArr = []
 
-# Read the JSON data
-try:
-    with open('dataPoints.json', 'r') as json_file:
-        dataDict = json.load(json_file)
-except IOError as e:
-    print(e)
-    exit(1)
 
-# Iterates through dict and assigns the elements to the proper array for plot
-for x in range(len(dataDict)):
-    resultArr.append(dataDict["Build"+str(x)]["Result"])
-    # Must convert the date from string back to date
-    dateArr.append(datetime.strptime(dataDict["Build"+str(x)]["Date"], '%Y-%m-%d'))
-    deviationArr.append(dataDict["Build"+str(x)]["PercentDeviation"])
+def getJsonData():
+    """Read the JSON data into dataDict"""
+    try:
+        with open('dataPoints.json', 'r') as json_file:
+            dataDict = json.load(json_file)
+    except IOError as e:
+        print(e)
+        exit(1)
 
-# Dont need this anymore, set the mem free...
-dataDict.clear()
+    # Iterates through dict and assigns the elements to the proper array for plot
+    for x in range(len(dataDict)):
+        resultArr.append(dataDict["Build"+str(x)]["Result"])
+        # Must convert the date from string back to date
+        dateArr.append(datetime.strptime(
+            dataDict["Build"+str(x)]["Date"], '%Y%m%d'))
+        deviationArr.append(dataDict["Build"+str(x)]["PercentDeviation"])
 
-# Plot Builder
-plt.plot(dateArr, deviationArr, 'r.')
-plt.title("Deviation from actual expected value")
-plt.ylabel("Percent Deviation")
-plt.xlabel("Build Date")
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.savefig('plot.png')
-plt.show()
+    # Dont need this anymore, set the mem free...
+    dataDict.clear()
 
-exit(0)
+
+def getDataDb():
+    """Calls the dataGenerator Module to get data from DB"""
+    dataGenerator.retrieveData(dataDict)
+
+    # Iterates through dict and assigns the elements to the proper array for plot
+    for x in range(len(dataDict)):
+        resultArr.append(dataDict[str(x+1)]["Result"])
+        # Must convert the date from string back to date
+        dateArr.append(datetime.strptime(
+            dataDict[str(x+1)]["Date"], '%Y%m%d'))
+        deviationArr.append(dataDict[str(x+1)]["PercentDeviation"])
+
+
+def plotBuilder():
+    """Builds the plot"""
+    # Plot Builder
+    plt.plot(dateArr, deviationArr, 'r.')
+    plt.title("Deviation from actual expected value")
+    plt.ylabel("Percent Deviation")
+    plt.xlabel("Build Date")
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig('plot.png')
+    plt.show()
+
+
+def main():
+    getDataDb()
+    plotBuilder()
+
+
+if __name__ == '__main__':
+    main()
